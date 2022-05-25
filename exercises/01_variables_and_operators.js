6328356980b6d691944430137df56bb1a40dae7e
Principe Matias <principematias23@gmail.com>
2022-05-25 19:35:42 -0300

    Cargando Primer Ejercicio, Resuelto

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,16 @@
+"use Strict"; //javascrip moderno On
+
+//exercise a
+let userName = 'Matias';
+let userSurname = 'Principe';
+let userConcat= userName.concat(" ",userSurname)
+console.log (userConcat)
+
+//exercise b
+
+let userEmail = 'principematias23@gmail.com'
+let software = 'gmail'
+let sum = new Number(userEmail.length + software)
+console.log (sum)
+
+
